3ffe860e7c137c044b56b69f58ab25d0cf7dd2d4
ocowchun <ocowchun@gmail.com>
2014-10-13 13:51:49 +0800

    New: add pla with random

--- homeworks/work1/work1.js
+++ homeworks/work1/work1.js
@@ -5,9 +5,18 @@ fs.readFile('hw1_15_train.dat.txt', 'utf8', function(err, data) {
 		return console.log(err);
 	}
 	var datas = txt2Datas(data);
-	excutePLA(datas);
-	// console.log(datas);
+
+	var updates = [];
+	_.times(2000, function() {
+		updates.push(randomPLA(datas));
+	});
+
+	var sum = _.reduce(updates, function(memo, num) {
+		return memo + num;
+	}, 0);
+	console.log(sum/2000)
 });
+
 
 function txt2Datas(text) {
 	var ar1 = text.split('\n');
@@ -24,16 +33,43 @@ function toNum(data) {
 	});
 }
 
-function excutePLA(datas) {
-	var weight = [0, 0, 0, 0, 0.5],
+function randomPLA(datas) {
+	var weight = [0, 0, 0, 0, 0],
 		isHalt = false,
 		y, x, currentData,
 		count = 0;
-	while (!isHalt && count++ < 5000) {
+	while (!isHalt && count++ < 100) {
+		datas = _.shuffle(datas);
+
 		var isMistake = _.find(datas, function(data) {
 			x = _.clone(data);
-			y = x.pop(); 
-			return (sign(weight, x) * y) < 0;
+			y = x.pop();
+
+			return (sign(weight, x) !== y);
+		});
+		if (isMistake) {
+			weight = updateWeight(weight, x, y);
+		} else {
+			isHalt = true;
+			// console.log(count);
+			console.log("you got it");
+			// console.log(weight);
+		}
+	}
+	return count
+}
+
+function excutePLA(datas) {
+	var weight = [0, 0, 0, 0, 0],
+		isHalt = false,
+		y, x, currentData,
+		count = 0;
+	while (!isHalt && count++ < 100) {
+		var isMistake = _.find(datas, function(data) {
+			x = _.clone(data);
+			y = x.pop();
+
+			return (sign(weight, x) !== y);
 		});
 		if (isMistake) {
 			weight = updateWeight(weight, x, y);
@@ -54,7 +90,8 @@ var sign = function(weight, x) {
 	for (i = 0; i < max; i++) {
 		result += weight[i] * x[i];
 	}
-	return result - _.last(weight);
+	var is_postive = (result + _.last(weight)) > 0;
+	return is_postive ? 1 : -1;
 }
 
 var updateWeight = function(weight0, x, y) {
@@ -63,11 +100,9 @@ var updateWeight = function(weight0, x, y) {
 		i;
 
 	for (i = 0; i < max; i++) {
-		weight1[i] = weight0[i] + x[i] * y * 0.1;
+		weight1[i] = weight0[i] + x[i] * y*0.5;
 	}
-	var threshold = _.last(weight0); // + _.last(weight0) * (y);
+	var threshold = _.last(weight0) + 0.5 * (y);
 	weight1.push(threshold);
 	return weight1;
 }
-
-// how to fix threshold
